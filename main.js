7cbf5b2ccf023f8cbca327ad5627ed5c0a8e8e3b
Douglas G. Moore <doug@dglmoore.com>
2018-09-17 00:02:05 +0000

    Initialize the package

---
+++ main.js
@@ -0,0 +1,7 @@
+// Copyright 2018. Douglas G. Moore. All rights reserved.
+// Use of this source code is governed by the MIT
+// license that can be found in the LICENSE file.
+
+(function() {
+	console.log('Caim v0.0.0');
+}());
