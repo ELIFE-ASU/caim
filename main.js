f35512026af8215196d2aa76c9819ab2230e00d0
Douglas G. Moore <doug@dglmoore.com>
2018-10-07 19:16:38 +0000

    Remove documentation comments.
    These are really just making everything noisy and slowing things
    down. For a project like this, where the end product is the goal,
    I think project-level documentation is better.

--- main.js
+++ main.js
@@ -1,54 +1,13 @@
-// Copyright 2018. Douglas G. Moore. All rights reserved.
-//
-// Use of this source code is governed by the MIT license that can be found in
-// the LICENSE file.
-
-// # The Caim UI
-//
-// Caim's UI is built on [electron](https://electronjs.org). This design
-// decision was based on two factors:
-//
-// 1. Electron is a reasonable cross-platform UI toolkit
-// 2. It is 100% Javascript + HTML + CSS
-//
-// The original version of [caim](https://github.com/elife-asu/caim-go) was
-// written using a Server + Client architecture, with the front-end implemented
-// with the standard web technologies. The hope is that this will make porting
-// caim to a standalone application using electron fairly straightforward.
-//
-// To start, I get `app`, `dialog`, `BrowserWindow` and `Menu` from electron.
-// The former will be our application object, and I'll use the latter to create
-// windows.
 const {app, dialog, BrowserWindow, Menu} = require('electron');
 const fs = require('fs');
 const path = require('path');
-
-// Next we create require the `Session` generator function from
-// `src/session.js`.
 const {Session} = require('./src/session');
 
-// ## Creating Windows
-//
-// I then need to create a global variable to keep track of our active window.
-// Caim is to be designed as a single-window application, so I don't need `win`
-// to be an array.
 let win = null;
 
-// From here I define a `create_window` function will will take care of
-// building a new Caim window.
 function create_window() {
-    // This entails initializing `win` with a new `BrowserWindow`.
     win = new BrowserWindow();
 
-    // Now that we have a window, I need to setup it's menu.  It consists of
-    // the following menu items:
-    //
-    // 1. File
-    //     * New Session
-    //     * Import Video
-    //     * Quit
-    // 2. View
-    //     * Fullscreen
     Menu.setApplicationMenu(new Menu.buildFromTemplate([
         {
             label: 'File',
@@ -83,21 +42,11 @@ function create_window() {
         }
     ]));
 
-    // The primary UI is provided in the `assets/startup.html` directory. To
-    // load it, I simply pass the path to the `loadFile` method of `win`.
     win.loadFile('assets/startup.html');
 
-    // I then register an `onclosed` event handler which sets our `win`
-    // variable to `null` so it can be garbage collected.
     win.on('closed', () => win = null);
 }
 
-// ## Handling App Events
-//
-// I create a window in response two events:
-//
-// 1. when the app's `'ready'` event fires
-// 2. when the application becomes activated and there is no window.
 app.on('ready', create_window);
 
 app.on('activate', function() {
@@ -106,22 +55,12 @@ app.on('activate', function() {
     }
 });
 
-// Finally, we quit the app if when the last window is close.
-//
-// **Note**: it is OS X standard operating procedure to keep the application
-// running after the last window is closed. Users have to explictly quit the
-// application.
 app.on('window-all-closed', function() {
     if (process.platform !== 'darwin') {
         app.quit();
     }
 });
 
-// ## Session Handling
-//
-// Our next step is to create the `session` variable which will store the
-// session details. This includes the path to the session, `path`, and the
-// session object itself, `data`.
 let session = {
     path: null,
     data: null,
@@ -130,15 +69,6 @@ let session = {
     }
 };
 
-// ## Handling Menu Events
-//
-// I need to be able to create new sessions, load old sessions, etc... That is
-// all handled via the menu. Each menu item, then, needs an associated event
-// handler.
-//
-// I started with the `File -> New Session` menu item, and created the
-// `new_session_dialog` handler. It starts a `dialog.showOpenDialog` allowing
-// the user to select a directory.
 function new_session_dialog(menuItem, browserWindow, event) {
     dialog.showOpenDialog(browserWindow, {
         title: 'Choose New Session Directory',
@@ -150,12 +80,8 @@ function new_session_dialog(menuItem, browserWindow, event) {
     }, new_session);
 }
 
-// The resulting paths are then passed to the `new_session` function.
 function new_session(session_path) {
-    // I first ensure that the user made a selection.
     if (session_path !== undefined) {
-        // Since a session must exist in a single directory,
-        // I also make sure they've only selected one path.
         if (session_path.length !== 1) {
             new_session_error({
                 message: 'Too many paths selected, select only one'
@@ -164,9 +90,6 @@ function new_session(session_path) {
         }
         session_path = session_path[0];
 
-        // I can then create the session directory if it doesn't exit, display
-        // an error if it isn't a directory, or display an error if it isn't
-        // empty.
         if (!fs.existsSync(session_path)) {
             fs.mkdirSync(session_path);
         } else if (!fs.statSync(session_path).isDirectory()) {
@@ -182,23 +105,16 @@ function new_session(session_path) {
             return;
         }
 
-        // At last, we have a valid session path. We can create a new session,
-        // and save it.
         session.path = session_path;
         session.data = Session();
         session.save();
 
-        // I then load the `assets/session.html` page into the UI.
         win.loadFile("assets/session.html");
 
-        // Finally, I enable the `File -> Import Video` menu item.
         app.getApplicationMenu().getMenuItemById('import-video').enabled = true;
     }
 }
 
-// The next handler is the `import_video_dialog` for the `File → Import Video`
-// menu item. It starts a `dialog.showOpenDialog` to let the user select video
-// files. The current implementation simply prints the selected filename.
 function import_video_dialog(menuItem, browserWindow, event) {
     dialog.showOpenDialog(browserWindow, {
         title: 'Choose a Video to Import',
@@ -230,13 +146,6 @@ function import_video(video_path) {
     }
 }
 
-// ## Error Dialogs
-//
-// Errors are likely time a program has to interact with humans. I need to be
-// able to easily report errors, in the form of dialog messages, to the users.
-//
-// Our first such message dialog is opened whenever an error occurs within the
-// `new_session` function.
 function new_session_error(options) {
     options = Object.assign({
         type: 'error',
@@ -256,9 +165,3 @@ function import_video_error(options) {
 
     dialog.showMessageBox(options);
 }
-
-// ## Conclusion
-//
-// That's all we need to create a basic UI with electron. As we develop the
-// project further, we'll need to extend electron's base menus with options for
-// loading videos and reloading sessions, but this is good for now.
--- src/session.js
+++ src/session.js
@@ -1,21 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 
-// ## Sessions
-//
-// Caim maintains all of its working data inside of directories which have a
-// particular internal structure. These directories are referred to as
-// "Sessions". A session can be created given a directory path.
 const Session = function() {
-    // Sessions as they are now do not store any information.
     const session = { };
 
-    // Sessions can save their internal state as a JSON object. Calling
-    // `save(session_path)` on a session will save the JSON object in a file
-    // `${session_path}/session.json`.
     session.save = function(session_path) {
-        // I have to make sure that the path is neither `undefined` nor empty
-        // (`''`).
         if (session_path === undefined) {
             throw new Error('no session path');
         } else if (session_path === '') {
