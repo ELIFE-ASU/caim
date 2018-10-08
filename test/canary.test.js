f4644cefadba41ee9f99c7acad686aaee9e1bade
Douglas G. Moore <doug@dglmoore.com>
2018-10-08 22:33:54 +0000

    Remove copyright headers

--- test/canary.test.js
+++ test/canary.test.js
@@ -1,5 +1,1 @@
-// Copyright 2018. Douglas G. Moore. All rights reserved.
-// Use of this source code is governed by the MIT
-// license that can be found in the LICENSE file.
-
 test('canary test', () => expect(1 + 2).toBe(3));
--- test/session.test.js
+++ test/session.test.js
@@ -1,6 +1,3 @@
-// Copyright 2018. Douglas G. Moore. All rights reserved.
-// Use of this source code is governed by the MIT
-// license that can be found in the LICENSE file.
 const {Session, load_session} = require('../src/session');
 const fs = require('fs');
 const path = require('path');
