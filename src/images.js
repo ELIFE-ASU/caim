3d2f13624f5d1b2918dfcf93d138a8f011e1e907
Douglas G. Moore <doug@dglmoore.com>
2018-10-25 14:20:04 +0000

    Read frames into Frame objects
    This cuts our static memory usage from ~50% to ~12% and keeps
    everything nice.

--- src/images.js
+++ src/images.js
@@ -1,3 +1,4 @@
+const Frame = require('./frame');
 const ffmpeg = require('fluent-ffmpeg');
 const ffmpeg_path = require('ffmpeg-static-electron').path;
 const fs = require('fs-extra');
@@ -24,7 +25,21 @@ async function extract_frames(video_path) {
 }
 
 async function load_frame(filename) {
-    return jimp.read(filename);
+    let img = await jimp.read(filename),
+        width = img.bitmap.width,
+        height = img.bitmap.height,
+        data = img.bitmap.data;
+
+    let frame = new Frame(width, height);
+    img.scan(0, 0, width, height, function(x, y, idx) {
+        let r = data[idx + 0],
+            g = data[idx + 1],
+            b = data[idx + 2];
+
+        frame.data[idx % 4] = Math.max(r, g, b);
+    });
+
+    return frame;
 }
 
 async function load_frames(frames_path) {
@@ -38,3 +53,4 @@ module.exports = {
     extract_frames: extract_frames,
     load_frames: load_frames
 };
+
