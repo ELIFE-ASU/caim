d1672a76001b4a8e09264c4cf31459ca824de292
Douglas G. Moore <doug@dglmoore.com>
2018-11-04 18:18:55 +0000

    Implement basic feature selection drawing

--- assets/caim.js
+++ assets/caim.js
@@ -1,10 +1,13 @@
 /* global Image */
 const {ipcRenderer} = require('electron');
 const d3 = require('d3');
+const {Point, Rectangle} = require('../src/selection');
 
 function Caim() {
     this.canvas = d3.select('#selection canvas').node();
     this.context = this.canvas.getContext('2d');
+    this.shapes = new Array();
+    this.color_scheme = d3.scaleOrdinal(d3.schemeCategory10);
     this.background = new Image();
     this.background.onload = (function(caim) {
         return function() {
@@ -19,6 +22,48 @@ function Caim() {
                 caim.canvas.height = this.height;
             }
 
+            let paint = false;
+
+            let add_click = function(x, y, start) {
+                if (start) {
+                    caim.shapes.push(Rectangle(Point(x, y), Point(x, y)));
+                } else {
+                    caim.shapes[caim.shapes.length - 1].add_point(Point(x, y));
+                }
+            };
+
+            caim.canvas.onmousedown = function(event) {
+                let x = event.pageX - this.offsetLeft,
+                    y = event.pageY - this.offsetTop;
+
+                paint = true;
+                add_click(x, y, true);
+                caim.redraw();
+            };
+
+            caim.canvas.onmousemove = function(event) {
+                if (paint) {
+                    let x = event.pageX - this.offsetLeft,
+                        y = event.pageY - this.offsetTop;
+
+                    add_click(x, y, false);
+                    caim.redraw();
+                }
+            };
+
+            caim.canvas.onmouseup = function() {
+                paint = false;
+                if (caim.shapes.length !== 0) {
+                    let shape = caim.shapes[caim.shapes.length - 1];
+                    if (shape.box.width === 0 || shape.box.height == 0) {
+                        caim.shapes.pop();
+                    }
+                }
+                caim.redraw();
+            };
+
+            caim.canvas.onmouseleave = caim.canvas.onmouseup;
+
             caim.redraw();
         };
     })(this);
@@ -31,6 +76,13 @@ Caim.prototype.init = function(uri) {
 Caim.prototype.redraw = function() {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     this.context.drawImage(this.background, 0, 0);
+    this.context.lineJoint = 'round';
+    this.context.lineWidth = 5;
+    this.shapes.forEach((shape, idx) => {
+        this.context.strokeStyle = this.color_scheme(idx);
+        this.context.fillStyle = this.color_scheme(idx);
+        shape.draw(this.context);
+    });
 };
 
 Caim.prototype.new_session = function() {
