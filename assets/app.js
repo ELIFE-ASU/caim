8c13f4843bff133f8fb6fe5fdb3e23ec856d5020
Douglas G. Moore <doug@dglmoore.com>
2018-12-02 00:08:58 -0700

    Move import options out of session div

--- assets/app.js
+++ assets/app.js
@@ -4,13 +4,13 @@ var caim = new Caim();
 
 ipcRenderer.on('load-session', function(event, path, metadata, uri) {
     d3.select('#startup').style('display', 'none');
-    d3.select('#session').style('display', 'block');
+    d3.select('#import').select('h2').text('Session Path: ' + path);
     d3.select('#session').select('h2').text('Session Path: ' + path);
     if (uri === undefined || uri === null) {
-        d3.select('#import-video').style('display', 'block');
+        d3.select('#import').style('display', 'block');
     } else {
-        d3.select('#import-video').style('display', 'none');
-        d3.select('#selection').style('display', 'block');
+        d3.select('#import').style('display', 'none');
+        d3.select('#session').style('display', 'block');
         caim.init(metadata, uri);
     }
 });
