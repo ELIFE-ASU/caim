fe080f85956781fc0c800c90a61cb5c18f81bafa
Douglas G. Moore <doug@dglmoore.com>
2018-12-16 23:16:41 -0700

    Implement the mutual info page

---
+++ assets/mutual_info.js
@@ -0,0 +1,111 @@
+const d3 = require('d3');
+const { ipcRenderer } = require('electron');
+
+(function() {
+    const color_scheme = d3.scaleOrdinal(d3.schemeCategory10);
+
+    var render_mutual_info = function(links) {
+        d3.select('#mutual-info').classed('phase--hidden', links.length === 0).html('');
+
+        var nodes = [];
+        links.forEach(function(link) {
+            link.source = nodes[link.source] || (nodes[link.source] = {name: link.source});
+            link.target = nodes[link.target] || (nodes[link.target] = {name: link.target});
+        });
+
+        var cell_width = 80,
+            cell_height = 25,
+            margin = 20,
+            padding = 5,
+            width = cell_width * (nodes.length + 1) + margin + padding,
+            height = cell_height * (nodes.length + 1) + margin + padding;
+
+        var local_scheme = d3.interpolateGnBu;
+
+        var svg = d3.select('#mi-results').append('svg')
+            .attr('title', 'Mutual Information')
+            .attr('version', 1.1)
+            .attr('xmlns', 'http://www.w3.org/2000/svg')
+            .attr('width', width)
+            .attr('height', height)
+            .attr('font-family', 'sans-serif');
+
+        var sources = svg.append('g')
+            .attr('font-size', 12)
+            .attr('transform', function() {
+                var x = margin, y = margin + cell_height + padding;
+                return 'translate(' + x + ',' + y + ')';
+            });
+
+        sources.selectAll('rect')
+            .data(links)
+            .enter().append('rect')
+            .attr('y', (d) => cell_height * d.source.name)
+            .attr('width', cell_width)
+            .attr('height', cell_height)
+            .attr('fill', (d) => color_scheme(d.source.name))
+            .attr('stroke', 'black');
+
+        sources.append('text')
+            .attr('transform', 'rotate(-90)')
+            .attr('fill', '#000000')
+            .attr('dy', '-0.5em')
+            .attr('text-anchor', 'end')
+            .text('Source');
+
+        var targets = svg.append('g')
+            .attr('font-size', 12)
+            .attr('transform', function() {
+                var x = margin + cell_width + padding, y = margin;
+                return 'translate(' + x + ',' + y + ')';
+            });
+
+        targets.selectAll('rect')
+            .data(links)
+            .enter().append('rect')
+            .attr('x', (d) => cell_width * d.target.name)
+            .attr('width', cell_width)
+            .attr('height', cell_height)
+            .attr('fill', (d) => color_scheme(d.target.name))
+            .attr('stroke', 'black');
+
+        targets.append('text')
+            .attr('fill', '#000000')
+            .attr('dy', '-0.5em')
+            .text('Target');
+
+        var cells = svg.append('g')
+            .attr('font-size', 12)
+            .attr('transform', function() {
+                var x = margin + cell_width + padding,
+                    y = margin + cell_height + padding;
+                return 'translate(' + x + ',' + y + ')';
+            });
+
+        cells.selectAll('rect')
+            .data(links)
+            .enter().append('rect')
+            .attr('class', 'mi-cell')
+            .attr('id', (d) => 'mi-' + d.source.name + '-' + d.target.name)
+            .attr('x', (d) => cell_width * d.target.name)
+            .attr('y', (d) => cell_height * d.source.name)
+            .attr('width', cell_width)
+            .attr('height', cell_height)
+            .attr('fill', (d) => local_scheme(d.value))
+            .attr('stroke', 'black');
+
+        cells.selectAll('text')
+            .data(links)
+            .enter().append('text')
+            .attr('x', (d) => cell_width * (d.target.name + 0.5))
+            .attr('y', (d) => cell_height * (d.source.name + 0.5))
+            .attr('dy', '0.25em')
+            .attr('text-anchor', 'middle')
+            .text((d) => (Math.round(d.value * 1000) / 1000) + ' bits');
+    };
+
+    ipcRenderer.on('mi', (event, links) => {
+        console.log(links);
+        render_mutual_info(links);
+    });
+}());
